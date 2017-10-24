5909c40c74bf7fbb00edc0428a888986aac6d950
Dane Thorsen <dthorsen@gannett.com>
2017-10-24 00:06:11 -0500

    Updated incidents list to use new style

--- src/renderer/components/Incidents.vue
+++ src/renderer/components/Incidents.vue
@@ -1,35 +1,17 @@
 <template>
-  <div class="container" id="incidents_container">
-    <div class="row">
-      <div class="col s9">
-        <div v-if="loading">Loading Incidents from statuspage.io API...
-          <div class="preloader-wrapper small active">
-            <div class="spinner-layer spinner-green-only">
-              <div class="circle-clipper left">
-                <div class="circle"></div>
-              </div>
-                <div class="gap-patch">
-                  <div class="circle"></div>
-              </div>
-              <div class="circle-clipper right">
-                <div class="circle"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="collection">
-          <router-link :to="{ name: 'UpdateIncident', params: { id: Item.id }}" v-for="Item in StatusPageIncidents">
-            <div class="container">
-              <div class="row">
-                <div class="col s6">{{ Item.name }}</div>
-                <div class="col s6">{{ Item.status }}</div>
-              </div>
-            </div>
-          </router-link>
-        </div>
-      </div>
-    </div>
-  </div>
+  <v-container grid-list-md>
+    <v-layout row wrap v-if="loading">
+      <v-flex xs12>Loading Incidents from statuspage.io API...
+        <v-progress-circular indeterminate v-bind:width="3"></v-progress-circular>
+      </v-flex>
+    </v-layout>
+    <router-link :to="{ name: 'UpdateIncident', params: { id: Item.id }}" v-for="Item in StatusPageIncidents" :key="Item.id">
+      <v-layout row xs12>
+        <v-flex xs6><v-subheader>{{ Item.name }}</v-subheader></v-flex>
+        <v-flex xs6><v-subheader>{{ Item.status }}</v-subheader></v-flex>
+      </v-layout>
+    </router-link>
+  </v-container>
 </template>
 
 <script>
