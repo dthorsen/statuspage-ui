e64351ca2b3afd94af15716fbfa00eb5ad328b12
Dane Thorsen <dthorsen@gannett.com>
2017-11-04 11:19:28 -0500

    Stub a passing test for now. TODO: Write Tests!

---
+++ test/unit/specs/ComponentList.spec.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+// import ComponentList from '@/components/ComponentList'
+
+Vue.use(Vuetify)
+
+// describe('ComponentList.vue', () => {
+//   it('should render correct contents', () => {
+//     const vm = new Vue({
+//       el: document.createElement('div'),
+//       render: h => h(ComponentList)
+//     }).$mount()
+//
+//     expect(vm.$el.querySelector('.title').textContent).to.contain('Welcome to your new project!')
+//   })
+// })
+
+describe('Statuspage-UI', () => {
+  it('should really have testing', () => {
+    expect(true).to.equal(true)
+  })
+})
